e8966d8979687172429e21fe52ac0291bc47315f
Michał <mglatki@gmail.com>
2021-12-25 18:19:56 +0100

    Created some markers

--- src/app/map/map.component.ts
+++ src/app/map/map.component.ts
@@ -22,6 +22,35 @@ export class MapComponent  implements AfterViewInit {
     });
 
     tiles.addTo(this.map);
+
+    this.initMarker(this.map);
+
+    this.addCustomMarkers(this.map);
+  }
+
+  initMarker(map: L.Map): L.Marker {
+    const userMarker = L.marker([39.8282, -98.5795]);
+
+    userMarker.addTo(map);
+
+    return userMarker;
+  }
+
+  addCustomMarkers(map: L.Map) : void {
+    const markers = new Array();
+
+    // for(let i = 0; i < markers.length; i++) {
+    //   markers.push(L.marker([(39 - i),(-98 - i)]).addTo(map));
+    // }
+
+    markers.push(L.marker([(39 - 1),(-98 - 1)]).addTo(map));
+    markers.push(L.marker([(39 - 2),(-98 - 2)]).addTo(map));
+    markers.push(L.marker([(39 - 3),(-98 - 3)]).addTo(map));
+    markers.push(L.marker([(39 - 4),(-98 - 4)]).addTo(map));
+    markers.push(L.marker([(39 - 5),(-98 - 5)]).addTo(map));
+
+
+    console.log(markers);
   }
 
   constructor() { }
